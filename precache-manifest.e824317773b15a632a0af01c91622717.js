dac584cd278434c3c9d410eaf2afdff6da6c75a6
Lorraine <Lorraine@Isaacs-MacBook-Pro.local>
2020-08-13 18:55:36 -0500

    Updates

---
+++ precache-manifest.e824317773b15a632a0af01c91622717.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d4d681e6b590f4e3349c2f6730df30b",
+    "url": "/404-not-found/index.html"
+  },
+  {
+    "revision": "b347d7ec4d81b2b80da2",
+    "url": "/404-not-found/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f15f982a9b06fdac1aec",
+    "url": "/404-not-found/static/js/2.a8948fc8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/404-not-found/static/js/2.a8948fc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b347d7ec4d81b2b80da2",
+    "url": "/404-not-found/static/js/main.9b2868b1.chunk.js"
+  },
+  {
+    "revision": "621ce4a0cda7d2b2b936",
+    "url": "/404-not-found/static/js/runtime-main.822a09ad.js"
+  },
+  {
+    "revision": "9f720a55ebc8ddf98c2593757aaa959c",
+    "url": "/404-not-found/static/media/Scarecrow.9f720a55.png"
+  }
+]);
